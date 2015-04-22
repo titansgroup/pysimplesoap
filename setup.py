457d16ffa70b2e8a6bc8bae9dd0b6d9933c0520c
Gustavo Armelin <gustavo.armelin@gmail.com>
2015-04-22 12:04:01 -0300

    added on pipy titans

--- setup.py
+++ setup.py
@@ -1,26 +1,22 @@
 #!/usr/bin/env python
 
-import setuptools
 from distutils.core import setup
 try:
-    import py2exe
     from nsis import build_installer
 except:
     build_installer = None
 
-from pysimplesoap import __version__, __author__, __author_email__, __license__
+from pysimplesoap import __author__, __author_email__, __license__
 
 # in the transition, register both:
-for name in ('soap2py', 'PySimpleSOAP'):
-    setup(
-        name=name,
-        version=__version__,
-        description='Python simple and lightweight SOAP Library',
-        author=__author__,
-        author_email=__author_email__,
-        url='http://code.google.com/p/pysimplesoap',
-        packages=['pysimplesoap'],
-        license=__license__,
-    #    console=['client.py'],
-        cmdclass={"py2exe": build_installer},
-    )
+setup(
+    name='tg-pysimplesoap',
+    version='0.0.1',
+    description='Python simple and lightweight SOAP Library',
+    author=__author__,
+    author_email=__author_email__,
+    url='http://code.google.com/p/pysimplesoap',
+    packages=['pysimplesoap'],
+    license=__license__,
+    cmdclass={"py2exe": build_installer},
+)
